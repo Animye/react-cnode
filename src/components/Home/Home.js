8be49a17a33dfc0d771f56e862e7ecc9ce626dfe
Animye <hy0620@163.com>
2018-12-21 10:32:43 +0800

    f

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,16 +2,11 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 import { Pagination } from 'antd'
+import Topics from '../Topics/Topics'
+import { Route } from 'react-router-dom'
 class Home extends Component {
   state = {
-    topics: [],
-    navArr: [
-      { type: 'all', txt: '全部' },
-      { type: 'good', txt: '精华' },
-      { type: 'share', txt: '分享' },
-      { type: 'ask', txt: '问答' },
-      { type: 'job', txt: '招聘' }
-    ]
+    topics: []
   }
   componentDidMount() {
     axios.get('https://cnodejs.org/api/v1/topics').then(res => {
@@ -20,48 +15,37 @@ class Home extends Component {
       })
     })
   }
-  choice = type => {
-    axios.get(`https://cnodejs.org/api/v1/topics?tab=${type}`).then(res => {
-      this.setState({
-        topics: res.data.data
-      })
-    })
-  }
-  page = page => {
-    // const { navArr } = this.state
-
-    // for (let i = 0; i < navArr.length; i++) {
-    //   console.log(this[i])
-    // }
-    axios
-      .get(`https://cnodejs.org/api/v1/topics?tab=all&page=${page}`)
-      .then(res => {
-        this.setState({
-          topics: res.data.data
-        })
-      })
-  }
+  // choice = type => {
+  //   axios.get(`https://cnodejs.org/api/v1/topics?tab=${type}`).then(res => {
+  //     this.setState({
+  //       topics: res.data.data
+  //     })
+  //   })
+  // }
+  // page = page => {
+  //   axios
+  //     .get(`https://cnodejs.org/api/v1/topics?tab=all&page=${page}`)
+  //     .then(res => {
+  //       this.setState({
+  //         topics: res.data.data
+  //       })
+  //     })
+  // }
   render() {
-    // const navArr = [
-    //   { type: 'all', txt: '全部' },
-    //   { type: 'good', txt: '精华' },
-    //   { type: 'share', txt: '分享' },
-    //   { type: 'ask', txt: '问答' },
-    //   { type: 'job', txt: '招聘' }
-    // ]
-    const { topics, navArr } = this.state
+    const navArr = [
+      { type: 'all', txt: '全部' },
+      { type: 'good', txt: '精华' },
+      { type: 'share', txt: '分享' },
+      { type: 'ask', txt: '问答' },
+      { type: 'job', txt: '招聘' }
+    ]
+    const { topics } = this.state
     const nav = navArr.map(e => (
       <li
-        // style={{
-        //   backgroundColor:
-        //     navArr.findIndex(a => a.type === e.type) !== -1
-        //       ? '#80bd01'
-        //       : '#f6f6f6'
-        // }}
         key={e.type}
-        onClick={() => {
-          this.choice(e.type)
-        }}
+        // onClick={() => {
+        //   this.choice(e.type)
+        // }}
       >
         {e.txt}
       </li>
@@ -70,19 +54,19 @@ class Home extends Component {
       topics.length === 0 ? (
         '请稍等'
       ) : (
-        <ul className='list'>
-          {topics.map(e => (
-            <li key={e.id}>{e.title} </li>
-          ))}
-        </ul>
+        <>
+          <Route component={Topics} path='/' exact />
+          <Route component={Topics} path='/:tab' />
+        </>
       )
+
     return (
       <Wrap>
         <nav>
           <Nav>{nav}</Nav>
         </nav>
         {list}
-        <Pagination defaultCurrent={1} total={50} onChange={this.page} />
+        {/* <Pagination defaultCurrent={1} total={50} onChange={this.page} /> */}
       </Wrap>
     )
   }
