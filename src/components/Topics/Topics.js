47f66183bb1fa07a47f62bdc8f7eee4b5714dcfd
Animye <hy0620@163.com>
2018-12-21 14:48:55 +0800

    no message

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 class Topics extends Component {
   state = {
     topics: []
@@ -24,7 +25,17 @@ class Topics extends Component {
       ) : (
         <ul className='list'>
           {topics.map(e => (
-            <li key={e.id}>{e.title}</li>
+            <li key={e.id}>
+              <img src={e.author.avatar_url} alt='' />{' '}
+              <span>
+                <span title='回复数'>{e.reply_count}</span>/{' '}
+                <span title='阅读量'>{e.visit_count}</span>
+              </span>
+              {e.top ? <span>置顶</span> : ''}
+              <Link to={`/topic/${e.id}`}>
+                {e.title} <span>1 小时</span>
+              </Link>
+            </li>
           ))}
         </ul>
       )
@@ -35,17 +46,22 @@ class Topics extends Component {
 export default Topics
 const Main = styled.div`
   .list {
-    padding: 0;
+    padding-left: 0;
   }
   .list > li {
     cursor: pointer;
     font-size: 16px;
-    line-height: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
     list-style: none;
     border: 1px solid #f0f0f0;
     :hover {
       background-color: #f6f6f6;
       text-decoration: underline;
     }
+  }
+  img {
+    width: 30px;
   }
 `
